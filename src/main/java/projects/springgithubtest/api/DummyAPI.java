c4dd2014fdf25d8dd1acdcb21f0f3a26b3768b63
2020026635 <dnwjd99@hanyang.ac.kr>
2024-06-09 16:16:10 +0900

    FEAT) dummy-and-dummy

--- src/main/java/projects/springgithubtest/api/DummyAPI.java
+++ src/main/java/projects/springgithubtest/api/DummyAPI.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import projects.springgithubtest.model.DummyModel;
 
+import java.util.HashMap;
+
 @RestController()
 public class DummyAPI {
 
@@ -13,6 +15,16 @@ public class DummyAPI {
     public ResponseEntity<DummyModel> dummy() {
         DummyModel dummyModel = new DummyModel("It is dummy api");
         return new ResponseEntity<>(dummyModel, HttpStatus.OK);
+    }
+
+    @GetMapping("/dummy-and-dummy")
+    public ResponseEntity<?> dummy2() {
+        DummyModel dummyModel1 = new DummyModel("It is first dummy api");
+        DummyModel dummyModel2 = new DummyModel("It is second dummy api");
+        HashMap<Long, DummyModel> hashMap = new HashMap<>();
+        hashMap.put(1L, dummyModel1);
+        hashMap.put(2L, dummyModel2);
+        return new ResponseEntity<>(hashMap, HttpStatus.OK);
     }
 
 }
